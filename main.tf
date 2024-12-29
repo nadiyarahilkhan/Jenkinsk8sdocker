a500c216d9806f44bde161a9795cec1b48e68ade
nadiyarahilkhan <120252295+nadiyarahilkhan@users.noreply.github.com>
2024-12-29 12:48:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,4 +6,13 @@ provider "google" {
 resource "google_container_cluster" "my-cluster" {
   name     = "my-cluster"
   location = "us-central1-a"
+
+  initial_node_count = 3  # Set a valid number (e.g., 3)
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
